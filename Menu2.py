4efdd936e3cc1ec227beb252c7b755ccc3be80bb
jimsyoung <122297150+jimsyoung@users.noreply.github.com>
2023-01-13 12:05:41 -0500

    Update Menu2.py

--- Menu2.py
+++ Menu2.py
@@ -1,17 +1,29 @@
+#import streamlit
+
+#streamlit.title('My Parents New Healthy Diner')
+
+#streamlit.header('Breakfast Menu')
+#streamlit.text('Omega 3 & Blueberry Oatmeal')
+#streamlit.text('Kale, Spinach & Rocket Smoothie')
+#streamlit.text('Hard-Boiled Free-Range Egg')
+
+
+
 import streamlit 
-streamlit.header(' My parents new healthy diner') 
-streamlit.title('Breakfast Favourites') 
+streamlit.header(' My Parents New Healthy Diner') 
+streamlit.title('Breakfast Menu') 
 streamlit.text ('🥣Omega-3 and blueberry oatmeal') 
 streamlit.text('🥬Kale,Spinach & Rocket smoothie') 
 streamlit.text('🐔Hard-boiled free range eggs') 
 streamlit.text('🥑🍞Avocado Toast') 
 streamlit.header('🍌🍐Build your own fruit smoothie🍑🥝') 
-import pandas 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
-my_fruit_list = my_fruit_list.set_index('Fruit') 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']) 
-fruits_to_show = my_fruit_list.loc[fruits_selected] 
-streamlit.dataframe(fruits_to_show) 
-streamlit.header("Fruityvice Fruit Advice!") 
-import requests fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon") 
-streamlit.text(fruityvice_response)
+
+#import pandas 
+#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
+#my_fruit_list = my_fruit_list.set_index('Fruit') 
+#fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']) 
+#fruits_to_show = my_fruit_list.loc[fruits_selected] 
+#streamlit.dataframe(fruits_to_show) 
+#streamlit.header("Fruityvice Fruit Advice!") 
+#import requests fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon") 
+#streamlit.text(fruityvice_response)
